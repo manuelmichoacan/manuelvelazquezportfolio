8e6640e9e92dbc46463f886f55c5db8a4eed332f
DESKTOP-JII64Q5\manue <mv6602@mx.att.com>
2026-05-06 12:59:10 -0600

    Se cabia la configuración de la lectura de amplify.

--- src/js/main.js
+++ src/js/main.js
@@ -74,7 +74,7 @@ require(['aws-amplify', './root'], function (AmplifyMod) {
     console.log("Estado de AmplifyMod:", AmplifyMod);
     console.log("Estado de window.Amplify:", window.Amplify);
     console.log("Estado de window.Amplify:", window.aws_amplify);
-    const Amplify = AmplifyMod || window.Amplify || window.aws_amplify;
+    const Amplify = AmplifyMod || window.Amplify;
     if (!Amplify || (typeof Amplify.configure !== 'function' && !Amplify.default)) {
         console.error("Crítico: Amplify no se cargó en el DOM.", AmplifyMod);
         return;
@@ -86,7 +86,8 @@ require(['aws-amplify', './root'], function (AmplifyMod) {
                 Cognito: {
                     userPoolId: "USER_POOL_ID", 
                     userPoolClientId: "APP_CLIENT_ID",
-                    region: "REGION_NAME"
+                    region: "REGION_NAME",
+                    mandatorySignIn: false
                 }
             }
         });
