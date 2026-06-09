df5ded542e69a850c199abe102b3593f238c6be4
DESKTOP-JII64Q5\manue <mv6602@mx.att.com>
2026-06-08 19:12:03 -0600

    Se le quita al sistema el control de los consecutivos.

--- src/js/viewModels/productos.js
+++ src/js/viewModels/productos.js
@@ -54,7 +54,6 @@ define(['knockout', 'ojs/ojarraydataprovider', 'services/ThemeService', 'ojs/ojk
         }
 
         const payload = {
-          id_producto: parseInt(self.formProduct.id()),
           descripcion_producto: self.formProduct.nombre().trim(),
           precio: parseFloat(String(self.formProduct.precio()).replace('$', '')),
           codigo_barras: String(self.formProduct.codigo_barras() || '').trim()
@@ -170,7 +169,6 @@ define(['knockout', 'ojs/ojarraydataprovider', 'services/ThemeService', 'ojs/ojk
         self.modalTitle('Modificar Producto');
         self.isEditMode(true);
         // Cargar datos seleccionados en los inputs de JET
-        self.formProduct.id(row.id_producto);
         self.formProduct.nombre(row.descripcion_producto);
         self.formProduct.precio(row.precio);
         self.formProduct.codigo_barras(row.codigo_barras || '');
